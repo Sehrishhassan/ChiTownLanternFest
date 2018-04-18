59c3cb68e3569937bc34f4fa9735feae8bd35a1c
wmsiddiqui <wmsiddiqui.6@gmail.com>
2018-04-17 23:48:05 -0500

    Added menu

--- script.js
+++ script.js
@@ -16,3 +16,41 @@ function animateUp(group) {
 
 animateDown(".group1");
 animateUp(".group2");
+
+$('.menu-toggle').click(function() {
+	"use strict";
+  	$('ul').toggleClass('opening');
+  	$(this).toggleClass('open');
+});
+
+$("#homeLink").click(function(e) { 
+	"use strict";
+	e.preventDefault();
+    $('html, body').animate({
+		scrollTop: $("#logoSection").offset().top
+    }, 1000);
+});
+
+$("#aboutLink").click(function(e) { 
+	"use strict";
+	e.preventDefault();
+    $('html, body').animate({
+		scrollTop: $("#aboutSection").offset().top
+    }, 1000);
+});
+
+$("#whatLink").click(function(e) { 
+	"use strict";
+	e.preventDefault();
+    $('html, body').animate({
+		scrollTop: $("#whatSection").offset().top
+    }, 1000);
+});
+
+$("#ticketsLink").click(function(e) { 
+	"use strict";
+	e.preventDefault();
+    $('html, body').animate({
+		scrollTop: $("#ticketsSection").offset().top
+    }, 1000);
+});
